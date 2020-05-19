1b4b68e34bf756dd2b7e39a36a0d6eefcea6060b
Gregory Hill <gregorydhill@outlook.com>
2020-05-19 17:11:12 +0100

    fully bake options with happy path test
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- src/Option.sol
+++ src/Option.sol
@@ -1,75 +1,129 @@
 pragma solidity ^0.5.15;
 
 import "@nomiclabs/buidler/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-// import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract PutOption {
     using SafeMath for uint;
 
-    // uint premium; // how much does 1 option cost
-    // uint total_underwritten; // the amount currently occupied
-
     IERC20 collateral;
+    IERC20 underlying;
+
     address owner;
+    address buyer;
+
+    // amount of collateral locked
+    uint256 public totalCollateral;
+
+    // amount of underlying asset locked
+    uint256 public totalUnderlying;
 
     // expiry block of the option
     uint256 public expiry;
 
-    // the amount supplied by underwrites
-    uint256 public totalSupply;
+    // the price to buy the option
+    uint256 public premium;
 
     // the strike price for 1 FlashBTC
     uint256 public strikePrice;
 
     constructor(
         IERC20 _collateral,
+        IERC20 _underlying,
         address _owner,
         uint256 _expiry,
+        uint256 _premium,
         uint256 _strikePrice
     ) public {
         require(_expiry > block.number, "Cannot init expired");
         require(_strikePrice > 0, "Requires non-zero strikePrice");
+        // check premium non-zero?
 
         collateral = _collateral;
+        underlying = _underlying;
         owner = _owner;
         expiry = _expiry;
+        premium = _premium;
         strikePrice = _strikePrice;
     }
 
     /**
-    * @dev Underwrite an option
-    * @param amount: erc-20 collateral
+    * @dev Claim the option for a fee
+    * @param amount: erc-20 underlying
     **/
-    function underwrite(uint256 amount) public {
-        require(!expired(), "Option has expired");
-        require(collateral.balanceOf(owner) >= amount, "Insufficient balance");
-        collateral.transferFrom(owner, address(this), amount);
-        totalSupply = totalSupply.add(amount);
+    function insure(uint256 amount) public {
+        require(!_expired(), "Option has expired");
+        require(!_locked(), "Option is locked");
+        require(amount > 0, "Requires non-zero amount");
+        // check that totalCollateral is sufficient
+        _payout(amount);
+
+        buyer = msg.sender;
+        require(underlying.balanceOf(buyer) >= amount, "Insufficient underlying");
+        require(collateral.balanceOf(buyer) >= premium, "Can't pay premium");
+        // take premium now
+        collateral.transferFrom(buyer, address(this), premium);
+        totalUnderlying = totalUnderlying.add(amount);
     }
 
     /**
-    * @dev Exercise an option
+    * @dev Underwrite an option, can be called multiple times
     * @param amount: erc-20 collateral
     **/
-    function exercise(uint256 amount) public {
-        // TODO: tx verify
-        require(!expired(), "Option has expired");
-        require(amount > 0, "Requires non-zero amount");
-        require(strikePrice > 0, "Requires non-zero strikePrice");
+    function underwrite(uint256 amount) public {
+        require(!_expired(), "Option has expired");
+        require(!_locked(), "Option is locked");
+        require(msg.sender == owner, "Unauthorized");
+        require(collateral.balanceOf(owner) >= amount, "Insufficient balance");
+        // we do the transfer here because it requires approval
+        collateral.transferFrom(owner, address(this), amount);
+        totalCollateral = totalCollateral.add(amount);
+    }
 
-        uint payout = amount.mul(strikePrice);
-        require(payout <= collateral.balanceOf(address(this)), "Not enough collateral");
-        collateral.transfer(msg.sender, payout);
+    /**
+    * @dev Exercise an option then self-destruct
+    **/
+    function exercise() public {
+        // TODO: tx verify
+        require(!_expired(), "Option has expired");
+        require(msg.sender == owner || msg.sender == buyer, "Unauthorized");
+
+        // send locked collateral to alice
+        uint payout = _payout(totalUnderlying);
+        collateral.transfer(buyer, payout);
+
+        // remainder should go to bob
+        collateral.transfer(owner, totalCollateral.sub(payout));
+        // premium isn't tracked in totalCollateral
+        collateral.transfer(owner, premium);
+
+        // recooperate some gas costs
+        selfdestruct(address(uint160(owner)));
+    }
+
+    /**
+    * @dev Computes the payout from the amount and the strikePrice
+    * @param amount: asset to exchange
+    */
+    function _payout(uint256 amount) private view returns (uint256) {
+        uint256 payout = amount.mul(strikePrice);
+        require(totalCollateral >= payout, "Insufficient collateral");
+        return payout;
     }
 
     /**
     * @dev Checks if the option has expired
     */
-    function expired() public view returns (bool) {
+    function _expired() public view returns (bool) {
         return (block.number >= expiry);
+    }
+
+    /**
+    * @dev Checks if the option is locked
+    */
+    function _locked() public view returns (bool) {
+        return buyer!=address(0);
     }
 }
 
--- test/options.test.ts
+++ test/options.test.ts
@@ -1,47 +1,118 @@
 import { ethers } from "@nomiclabs/buidler";
 import { Signer, Wallet } from "ethers";
 import chai from "chai";
-import { deployContract, solidity } from "ethereum-waffle";
-import CollateralArtifact from "../artifacts/Collateral.json";
-import OptionPoolArtifact from "../artifacts/OptionPool.json";
-import { Collateral } from "../typechain/Collateral";
+import { solidity } from "ethereum-waffle";
+import { Mintable } from "../typechain/Mintable";
+import { MintableFactory } from "../typechain/MintableFactory";
 import { OptionPool } from "../typechain/OptionPool";
+import { OptionPoolFactory } from "../typechain/OptionPoolFactory";
 import { PutOption } from "../typechain/PutOption";
+import { PutOptionFactory } from "../typechain/PutOptionFactory";
 
 chai.use(solidity);
 const { expect } = chai;
 
-async function getOption(address: string): Promise<PutOption> {
-  const factory = await ethers.getContract("PutOption");
+async function getOption(address: string, signer: Signer): Promise<PutOption> {
+  const factory = (await ethers.getContract("PutOption")).connect(signer);
   return factory.attach(address) as PutOption;
 }
 
+interface Callable {
+  address: string;
+}
+
+interface Attachable<C> {
+  attach(addr: string): C;
+}
+
+function call<A extends Callable, B extends Attachable<A>>(contract: A, factory: new (from: Signer) => B, signer: Signer): A {
+  let _factory = new factory(signer);
+  return _factory.attach(contract.address);
+}
+
 describe("Options", () => {
-  let signers: Signer[];
-  let collateral: Collateral;
+  let alice: Signer;
+  let bob: Signer;
+
+  let aliceAddress: string;
+  let bobAddress: string;
+
+  let collateral: Mintable;
+  let underlying: Mintable;
+
   let optionPool: OptionPool;
 
-  it("create and exercise put option", async () => {
-    signers = await ethers.signers();
-    
-    let signer = await signers[0].getAddress();
-    collateral = await deployContract(<Wallet>signers[0], CollateralArtifact, []) as Collateral;
-    let amount = 100;
-    await collateral.mint(signer, amount);
-    expect((await collateral.balanceOf(signer)).toNumber()).to.eq(amount);
-    expect((await collateral.totalSupply()).toNumber()).to.eq(amount);
-    
-    optionPool = await deployContract(<Wallet>signers[0], OptionPoolArtifact, [collateral.address]) as OptionPool;
-    await optionPool.create(20, 1);
+  beforeEach(async () => {
+    let signers = await ethers.signers();
+    alice = signers[0];
+    bob = signers[1];
+
+    aliceAddress = await alice.getAddress()
+    bobAddress = await bob.getAddress()
+
+    let mintableFactory = new MintableFactory(alice);
+    collateral = await mintableFactory.deploy();
+    underlying = await mintableFactory.deploy();
+
+    let optionFactory = new OptionPoolFactory(bob);
+    optionPool = await optionFactory.deploy(collateral.address, underlying.address);
+  });
+
+  const put = async function(
+    collateralAmount: number,
+    underlyingAmount: number,
+    premium: number,
+    strikePrice: number,
+  ) {
+    // bob needs collateral >= the insured amount
+    await collateral.mint(bobAddress, collateralAmount);
+    expect((await collateral.balanceOf(bobAddress)).toNumber()).to.eq(collateralAmount);
+
+    // alice needs fees to pay premium
+    await collateral.mint(aliceAddress, premium);
+    expect((await collateral.balanceOf(aliceAddress)).toNumber()).to.eq(premium);
+
+    // alice needs to have underlying
+    await underlying.mint(aliceAddress, underlyingAmount);
+    expect((await underlying.balanceOf(aliceAddress)).toNumber()).to.eq(underlyingAmount);
+
+    await optionPool.create(20, premium, strikePrice);
     
     let address = await optionPool.options(0);
-    await collateral.approve(address, amount);
-    let option = await getOption(address);
-    await option.underwrite(amount);
-    expect((await collateral.balanceOf(signer)).toNumber()).to.eq(0);
-    expect((await option.totalSupply()).toNumber()).to.eq(amount);
+    let option = await getOption(address, alice);
 
-    await option.exercise(100);
-    expect((await collateral.balanceOf(signer)).toNumber()).to.eq(amount);
+    // alice is not owner, therefore cannot underwrite
+    await expect(call(option, PutOptionFactory, alice).underwrite(collateralAmount)).to.be.reverted;
+
+    // bob should approve collateral transfer and underwrite
+    await call(collateral, MintableFactory, bob).approve(address, collateralAmount);
+    await call(option, PutOptionFactory, bob).underwrite(collateralAmount);
+
+    // balances should be adjusted accordingly
+    expect((await collateral.balanceOf(await bob.getAddress())).toNumber()).to.eq(0);
+    expect((await option.totalCollateral()).toNumber()).to.eq(collateralAmount);
+
+    return option;
+  };
+
+  it("create and exercise put option", async () => {
+    let collateralAmount = 100;
+    let underlyingAmount = 100;
+    let premium = 20;
+    let strikePrice = 1;
+
+    let option = await put(collateralAmount, underlyingAmount, premium, strikePrice);
+
+    // alice now claims option by paying premium
+    await call(collateral, MintableFactory, alice).approve(option.address, premium);
+    await call(option, PutOptionFactory, alice).insure(underlyingAmount);
+
+    expect((await option.totalUnderlying()).toNumber()).to.eq(underlyingAmount);
+
+    await call(option, PutOptionFactory, alice).exercise();
+
+    expect((await collateral.balanceOf(aliceAddress)).toNumber()).to.eq(collateralAmount);
+    expect((await collateral.balanceOf(bobAddress)).toNumber()).to.eq(premium);
   });
+
 });
