9177c4ee468ed3a7eb0991319ee5b93cd7da859f
Dominik Harz <dominik@interlay.io>
2020-05-21 16:46:30 +0900

    add determinstic builds

--- buidler.config.ts
+++ buidler.config.ts
@@ -5,7 +5,10 @@ usePlugin("@nomiclabs/buidler-ganache");
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("buidler-typechain");
 
-const config: BuidlerConfig = {
+// environment
+const INFURA_ID = process.env.INFURA_XFLASH_ID;
+
+const config /*: BuidlerConfig*/ = {
 	defaultNetwork: "buidlerevm",
 	solc: {
 		version: '0.5.15',
@@ -15,9 +18,20 @@ const config: BuidlerConfig = {
 		tests: './test',
 	},
 	typechain: {
-    	outDir: "typechain",
-    	target: "ethers"
+    	outDir: 'typechain',
+    	target: 'ethers'
   	},
+    networks: {
+        buidlerevm : {},
+        ganache: {
+            url: 'http://127.0.0.1:8545',
+            // fork: 'https://ropsten.infura.io/v3/${ INFURA_ID }',
+            mnemonic: 'lion album emotion suffer october belt uphold mind chronic stool february flag',
+            network_id: 3,
+            timeout: 0,
+            logger: console,
+        },
+    },
 };
 
 export default config;
