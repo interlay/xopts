2c4d108d0b6d127eadbddf7490449b0c01be1d2f
Dominik Harz <dominik@interlay.io>
2020-05-22 19:41:24 +0900

    change from mintable to collateral

--- test/options.test.ts
+++ test/options.test.ts
@@ -2,8 +2,8 @@ import { ethers } from "@nomiclabs/buidler";
 import { Signer } from "ethers";
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
-import { Mintable } from "../typechain/Mintable";
-import { MintableFactory } from "../typechain/MintableFactory";
+import { Collateral } from "../typechain/Collateral";
+import { CollateralFactory } from "../typechain/CollateralFactory";
 import { OptionPool } from "../typechain/OptionPool";
 import { OptionPoolFactory } from "../typechain/OptionPoolFactory";
 import { PutOption } from "../typechain/PutOption";
@@ -17,7 +17,7 @@ chai.use(solidity);
 const { expect } = chai;
 
 async function getOption(address: string, signer: Signer): Promise<PutOption> {
-  const factory = (await ethers.getContract("Option")).connect(signer);
+  const factory = (await ethers.getContract("PutOption")).connect(signer);
   return factory.attach(address) as PutOption;
 }
 
@@ -52,7 +52,7 @@ describe("Options", () => {
   let bobAddress: string;
   let charlieAddress: string;
 
-  let collateral: Mintable;
+  let collateral: Collateral;
   let optionPool: OptionPool;
 
   let btcAddress = "0x66c7060feb882664ae62ffad0051fe843e318e85";
@@ -78,7 +78,7 @@ describe("Options", () => {
             alice
         );
     } else {
-        let mintableFactory = new MintableFactory(alice);
+        let mintableFactory = new CollateralFactory(alice);
         collateral = await mintableFactory.deploy();
     }
 
@@ -130,7 +130,7 @@ describe("Options", () => {
     let option = await getOption(address, alice);
 
     // bob should approve collateral transfer and underwrite
-    await call(collateral, MintableFactory, bob).approve(address, collateralAmount);
+    await call(collateral, CollateralFactory, bob).approve(address, collateralAmount);
     await call(option, PutOptionFactory, bob).underwrite(collateralAmount, btcAddress);
 
     await call(option, PutOptionFactory, bob).approve(address, collateralAmount);
@@ -168,7 +168,7 @@ describe("Options", () => {
     let option = await put(collateralAmount, underlyingAmount, premium, strikePrice, 20);
 
     // alice now claims option by paying premium
-    await call(collateral, MintableFactory, alice).approve(option.address, premium * underlyingAmount);
+    await call(collateral, CollateralFactory, alice).approve(option.address, premium * underlyingAmount);
     await call(option, PutOptionFactory, alice).insure(underlyingAmount, bobAddress);
     expect((await collateral.balanceOf(bobAddress)).toNumber()).to.eq(premium * underlyingAmount);
 
@@ -195,7 +195,7 @@ describe("Options", () => {
     expect((await option.balanceOf(charlieAddress)).toNumber()).to.eq(collateralAmount);
 
     // alice claims option by paying premium
-    await call(collateral, MintableFactory, alice).approve(option.address, premium * underlyingAmount);
+    await call(collateral, CollateralFactory, alice).approve(option.address, premium * underlyingAmount);
     let insureCall = call(option, PutOptionFactory, alice).insure(underlyingAmount, charlieAddress);
     await expect(insureCall).to.be.reverted;
 
@@ -228,7 +228,7 @@ describe("Options", () => {
     let option = await put(collateralAmount, underlyingAmount, premium, strikePrice, expiry);
 
     // alice now claims option by paying premium
-    await call(collateral, MintableFactory, alice).approve(option.address, premium * underlyingAmount);
+    await call(collateral, CollateralFactory, alice).approve(option.address, premium * underlyingAmount);
     await call(option, PutOptionFactory, alice).insure(underlyingAmount, bobAddress);
     expect((await collateral.balanceOf(bobAddress)).toNumber()).to.eq(premium * underlyingAmount);
 
@@ -265,7 +265,7 @@ describe("Options", () => {
     let option = await put(collateralAmount, underlyingAmount, premium, strikePrice, expiry);
 
     // alice now claims option by paying premium
-    await call(collateral, MintableFactory, alice).approve(option.address, premium * underlyingAmount);
+    await call(collateral, CollateralFactory, alice).approve(option.address, premium * underlyingAmount);
     await call(option, PutOptionFactory, alice).insure(underlyingAmount, bobAddress);
     expect((await collateral.balanceOf(bobAddress)).toNumber()).to.eq(premium * underlyingAmount);
 
