8d3fae256a57f46f5bace854bf60020ea15950b9
alexei.zamyatin <alexei.zamyatin@gmail.com>
2020-05-22 10:33:48 +0200

    started updating UI

---
+++ dapp_new/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const ReactDOM = require("react-dom");
+const Hello_1 = require("./components/Hello");
+ReactDOM.render(React.createElement(Hello_1.Hello, { compiler: "TypeScript", framework: "React" }), document.getElementById("example"));
+//# sourceMappingURL=index.js.map
