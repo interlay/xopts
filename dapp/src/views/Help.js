397ad7995041a56133b09eed0810aafdf56ab36d
Dominik Harz <dominik@interlay.io>
2020-06-09 16:05:05 +0900

    add help page

--- dapp/src/views/Help.js
+++ dapp/src/views/Help.js
@@ -4,6 +4,9 @@ import { Image, Container, Row, Col, Button } from "react-bootstrap";
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 import HelpBuy from '../components/HelpBuy.js';
+import HelpExercise from '../components/HelpExercise.js';
+import HelpSell from '../components/HelpSell.js';
+import HelpRefund from '../components/HelpRefund.js';
 
 class Help extends Component {
 
@@ -14,10 +17,31 @@ class Help extends Component {
         <section className="jumbotron text-center">
           <div className="container">
             <h2>XOpts User Guide</h2>
-            <h3 className="lead text-muted">Coming soon.</h3>
+            <h3 className="lead text-muted">Learn how to buy and sell BTC options.</h3>
           </div>
         </section>
-        <Container fluid style={{height: "80vh"}}>
+        <Container fluid>
+          <section class="col-xl-8 offset-xl-2">
+            <h2>Guides</h2>
+                <div>
+                  <li class="list-group-item border-0"><a href="#buy-help" class="list-group-item-action">How to Buy Options</a></li>
+                  <li class="list-group-item border-0"><a href="#execute-help" class="list-group-item-action">How to Exercise Options</a></li>
+                  <li class="list-group-item border-0"><a href="#sell-help" class="list-group-item-action">How to Sell Options</a></li>
+                  <li class="list-group-item border-0"><a href="#refund-help" class="list-group-item-action">How to Refund Expired Options</a></li>
+                </div>
+          </section>
+          <section class="mt-5">
+            <HelpBuy />
+          </section>
+          <section class="mt-5">
+            <HelpExercise />
+          </section>
+          <section class="mt-5">
+            <HelpSell />
+          </section>
+          <section class="mt-5">
+            <HelpRefund />
+          </section>
         </Container>
       </div>
     );
