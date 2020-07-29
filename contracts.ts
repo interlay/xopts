3ea6f158107004815cfaa3d4113c03af8cdbeef1
Dominik Harz <dominik@interlay.io>
2020-07-29 11:50:09 +0900

    remove old contract addresses

--- contracts.ts
+++ contracts.ts
@@ -1,15 +1,5 @@
 export const contracts = {
     ropsten: {
-      dai: "0x31F42841c2db5173425b5223809CF3A38FEde360",
-      proxyRegistry: "0x1b8357eB14Dd29e4D29AC163342Ee838DeEBCC7f",
-      dssProxyActions: "0xf2A4966bE1e633Fd3A4b7011Ab543b0AA5050047",
-      cdpManager: "0x033b7629CeC52a41712C362868f6cd70aEFc0545",
-      jug: "0x4bc636a867E8B9275aB022C1Ef44d6B24bc65b8d",
-      join_eth_A: "0xa885b27E8754f8238DBedaBd2eae180490C341d7",
-      join_dai: "0xA0b569e9E0816A20Ab548D692340cC28aC7Be986",
-      dai_account: "0xd422104e6310367abe12456fc6017513601e5732",
-      collateral: "0xEa81e6a0b47b724C38cc56583310EAc6D26f3dD2",
-      relay: "0xCB917F7fE94aB244F46778af447b9b9230330f19",
-      optionPool: "0x07633ee498C7084Cf7B47c886ecD27A7E57C6907"
+      dai: ""
     }
 };
